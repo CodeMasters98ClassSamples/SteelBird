a0ded16a194cb166abd700f67c6bb6970c54b033
Parham Darvishi <darvishiparham14@gmail.com>
2025-07-21 13:39:57 +0330

    Add New Job

---
+++ SteelBird.Infrastructure/BackgroundJobs/UpdatePriceJob.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SteelBird.Infrastructure.BackgroundJobs;
+
+public class UpdatePriceJob : BackgroundService
+{
+    private readonly ILogger<UpdatePriceJob> _logger;
+    private readonly IServiceProvider _serviceProvider;
+    public UpdatePriceJob(
+        ILogger<UpdatePriceJob> logger,
+        IServiceProvider serviceProvider)
+    {
+        _logger = logger;
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        //Business Logic
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            // Wait with cancellation support
+            await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+        }
+    }
+}
--- SteelBird.Presentation.API/Program.cs
+++ SteelBird.Presentation.API/Program.cs
@@ -16,11 +16,17 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 var environment = builder.Environment.EnvironmentName;
+if (string.IsNullOrEmpty(environment))
+{
+    //Log
+}
 
 var useInMemoryDatabase = builder.Configuration.GetValue<bool>("UseInMemoryDatabase");
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
@@ -89,10 +95,13 @@ if (app.Environment.IsDevelopment())
 {
     using (var scope = app.Services.CreateScope())
     {
+        //Role Managnet
         var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
         await roleManager.CreateAsync(new IdentityRole(UserRoles.USER.ToString()));
         await roleManager.CreateAsync(new IdentityRole(UserRoles.ADMIN.ToString()));
 
+
+        //UserManagment
         var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
         var user = new ApplicationUser
         {
@@ -104,7 +113,9 @@ if (app.Environment.IsDevelopment())
         var result = await userManager.CreateAsync(user, "Pp@1234");
         await userManager.AddToRoleAsync(user, UserRoles.ADMIN.ToString());
 
+        //Product Manage
         var context = scope.ServiceProvider.GetRequiredService<CoreDatabaseContext>();
+
         context.Products.AddRange(
             new Product { Id = 1, Name = "Notebook", Price = 5.99m,Barcode = "1", Description = "1" },
             new Product { Id = 2, Name = "Pen", Price = 1.49m, Barcode = "2",Description = "2" }
